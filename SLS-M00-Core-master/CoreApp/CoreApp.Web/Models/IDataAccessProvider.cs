ebab479fdda756178985e0845831a9a14d889330
Synastra <aloy.g95@gmail.com>
2021-01-13 21:24:10 +0800

    inserting data to db example

---
+++ SLS-M00-Core-master/CoreApp/CoreApp.Web/Models/IDataAccessProvider.cs
@@ -0,0 +1,14 @@
+﻿using CoreApp.Web.Models;
+using System.Collections.Generic;
+
+namespace CoreApp.Web.DataAccess
+{
+    public interface IDataAccessProvider
+    {
+        void AddPatientRecord(Patient patient);
+        void UpdatePatientRecord(Patient patient);
+        void DeletePatientRecord(string id);
+        Patient GetPatientSingleRecord(string id);
+        List<Patient> GetPatientRecords();
+    }
+}
---
+++ SLS-M00-Core-master/CoreApp/CoreApp.Web/Models/Patient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace CoreApp.Web.Models
+{
+    public class Patient
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string age { get; set; }
+        public string gender { get; set; }
+
+
+    }
+}
